4e0d3932c6ba0d76387c448c2a5d99e42b0cc16d
Tomasz Morawski <romen3@gmail.com>
2017-02-06 23:10:29 +0100

    Dodane chowanie/pojawianie się pasków

--- Assets/ShowStats.cs
+++ Assets/ShowStats.cs
@@ -4,10 +4,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ShowStats : MonoBehaviour {
-    public float hiddenX = 234.6f;
-    public float shownX = 85.4f;
+    public float hiddenX = 80.6f;
+    public float shownX = -76.7f;
 
-    private bool shown = false;
+    private bool shown = true;
     private ObjectManager ObjectManager;
 
     private void toggleShown() {
@@ -32,8 +32,8 @@ public class ShowStats : MonoBehaviour {
         bool tmpShown = shown;
         foreach (var StatBar in ObjectManager.BarsList) {
             Debug.Log("Starting animation for bar " + StatBar);
-            StartCoroutine(ToggleBar(StatBar, shown));
-            yield return new WaitForSeconds(0.3f);
+            StartCoroutine(ToggleBar(StatBar, tmpShown));
+            yield return new WaitForSeconds(0.05f);
         }
         toggleShown();
         Debug.Log("Finished animation!");
@@ -42,21 +42,21 @@ public class ShowStats : MonoBehaviour {
     private IEnumerator ToggleBar(GameObject statBar, bool shown) {
         if (shown) {
             RectTransform statBarTransform = statBar.GetComponent<RectTransform>();
-            while (statBarTransform.localPosition.x < hiddenX) {
-                float oldX = statBarTransform.localPosition.x;
-                float oldY = statBarTransform.localPosition.y;
-                statBarTransform.localPosition.Set(oldX + 5, oldY, 0);
-                yield return new WaitForSeconds(0.1f);
+            while (statBarTransform.anchoredPosition.x < hiddenX) {
+                float oldX = statBarTransform.anchoredPosition.x;
+                float oldY = statBarTransform.anchoredPosition.y;
+                statBarTransform.anchoredPosition = new Vector2(oldX + 10, oldY);
+                yield return new WaitForSeconds(0.005f);
             }
         }
         else {
             RectTransform statBarTransform = statBar.GetComponent<RectTransform>();
-            while (statBarTransform.localPosition.x > hiddenX) {
-                Debug.Log(statBarTransform.localPosition);
-                float oldX = statBarTransform.localPosition.x;
-                float oldY = statBarTransform.localPosition.y;
-                statBarTransform.localPosition.Set(oldX + 5, oldY, 0);
-                yield return new WaitForSeconds(0.1f);
+            while (statBarTransform.anchoredPosition.x > shownX) {
+                float oldX = statBarTransform.anchoredPosition.x;
+                float oldY = statBarTransform.anchoredPosition.y;
+                statBarTransform.anchoredPosition = new Vector2(oldX - 10, oldY);
+                Debug.Log(statBarTransform.anchoredPosition);
+                yield return new WaitForSeconds(0.005f);
             }
         }
     }
